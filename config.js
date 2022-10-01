f0dfe71ca41782f762fc5a36520cbf95419ed00d
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-01 10:00:57 +0800

    config.js

--- config.js
+++ config.js
@@ -19,25 +19,25 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['62895327999292']
+global.ownernomer = "62895327999292"
+global.premium = ['62895327999292']
+global.packname = 'WannTrue BOTZ'
+global.author = 'WannTrue'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 global.mess = {
-    success: 'Done',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Loading...',
-    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    success: 'Done kontol',
+    admin: 'Lu bukan admin ngentod!',
+    botAdmin: 'Gw bukan admin anj!',
+    premime: 'Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Lu bukan owner gw setan',
+    group: 'Cuma group dek!',
+    private: 'Hanya Untuk Private Chat!',
+    bot: 'Khusus Pengguna Nomor Bot',
+    wait: '_*MOHON MENUNGGU YA KONTOL*_',
+    endLimit: 'Limit Harian Lu Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
 global.limitawal = {
     premium: "Infinity",
