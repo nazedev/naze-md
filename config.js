d06766d4d93779f82d988c50911a587dbc45bcce
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-10-22 20:59:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['60102810046']
+global.ownernomer = "6102810046"
+global.premium = ['60102810046']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'lynx.man'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -62,13 +62,13 @@ global.limitawal = {
 }
 global.thumb = fs.readFileSync('./media/image/naze.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
-global.mygit = 'https://github.com/nazedev'
-global.myyt = 'https://youtube.com/c/Nazedev'
-global.myytv = 'https://youtu.be/ey7qLuRBm5Y'
-global.mygc = "https://chat.whatsapp.com/Khpnk6svwIhCIvx91Xi0Vf"
-global.botname = 'Naze Dev'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE'
+global.mygit = 'https://github.com/ai-man-123'
+global.myyt = '-'
+global.myytv = '-'
+global.mygc = "-"
+global.botname = 'lynx.man'
+global.akulaku = 'Bot By Aiman'
+global.ytname = '-'
 
 
 let file = require.resolve(__filename)
